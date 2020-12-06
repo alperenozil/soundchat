1650696132df3585c480e4fe4c4b8053e24e716f
Necdet Alperen Özil <48528832+alperenozil@users.noreply.github.com>
2020-12-06 11:14:07 +0300

    twitter login is implemented

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,17 @@
 import '../../firebase/firebaseConfiguration';
 import { assignClick, initializeSigninButtons } from './utilities';
-import { googleSignin, signOut } from '../../firebase/firebaseAuthentication';
+import { googleSignin, signOut, twitterSignin } from '../../firebase/firebaseAuthentication';
+import { writeSongToFirestore } from '../../firebase/firebaseRepository'
 initializeSigninButtons();
 assignClick('signin-google', googleSignin);
 assignClick('appbar-signout-button',signOut);
+assignClick('signin-twitter',twitterSignin); 
+const createTuneForm = document.getElementById('add-tune-form');
+if (createTuneForm) {
+    createTuneForm.onsubmit = (event) => {
+        event.preventDefault();
+        const songArtist = event.target['artist-input'].value;
+        const songTitle=event.target['song-title-input'].value;
+        writeSongToFirestore(songArtist,songTitle);
+    }
+}
